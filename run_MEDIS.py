6b312956d45a2c31f374c7f554ede249f80d7782
Briley Lewis <blewis@new-macbook.home>
2020-08-26 14:22:20 -0700

    wrapper and bash script for MEDIS on Hoffman2

---
+++ run_MEDIS.py
@@ -0,0 +1,42 @@
+
+import sys
+sys.path.insert(0,'/u/home/b/blewis34/MEDIS/Proper')
+sys.path.insert(0,'/u/home/b/blewis34/MEDIS/Proper/proper')
+sys.path.insert(0,'/u/home/b/blewis34/MEDIS/medis')
+sys.path.insert(0,'/u/home/b/blewis34/MEDIS')
+import medis.Detector.get_photon_data as gpd
+from medis.params import ap, tp, sp, iop
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+##set astro parameters
+ap.companion=False #is there a planet in there?
+ap.exposure_time = 0.001 #exposure time in [???units]
+ap.numframes = 1
+ap.nwsamp = 1 #number of wavefronts in proper to sample from
+
+##set telescope parameters
+tp.use_spiders =False #spiders off/on
+tp.use_ao = True #AO off/on
+tp.piston_error = False #piston error off/on
+tp.use_coron = False #coronagraph off/on
+tp.detector = 'ideal'
+tp.check_args()
+
+##set simulation parameters
+sp.show_wframe = False
+sp.save_obs = False
+sp.num_processes = 1
+sp.save_locs = np.array(['add_atmos', 'coronagraph'])
+sp.return_E = True
+
+occulter_types = ['None', 'Vortex']
+
+for i in range(2):
+    iop.update("mod_barebones/coron_%s" % occulter_types[i])
+    tp.occulter_type = occulter_types[i]  # occulter type - vortex, none, gaussian
+
+    if __name__ == '__main__':
+        save_E_fields = gpd.run_medis(plot=False)
+        print(save_E_fields.shape)  # ap.numframes x len(sp.save_locs) x (1 star + # of companions) x ap.nwsamp x ap.grid_size x ap.grid_size

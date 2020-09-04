7a9ade13162bf62d2aa3abbd5c374dd3141e4f31
Briley Lewis <blewis@new-macbook.home>
2020-09-04 13:49:21 -0700

    changed runtimes

--- MEDIS_nc.py.cmd
+++ MEDIS_nc.py.cmd
@@ -14,7 +14,7 @@
 #  program output  = Specified by user program
 #  Resources requested
 #
-#$ -l h_data=32000M,h_rt=12:00:00,exclusive
+#$ -l h_data=16000M,h_rt=2:00:00
 # #
 #  Name of application for log
 #$ -v QQAPP=job
